5f2a89b6cfd2e5661b869321debcafd05576d132
PublicVar <dev.publicvar@gmail.com>
2017-10-26 14:10:19 +0400

    Edit IsMyAnimal specs. Remove the impossible specs

--- tests/IsMyAnimal.spec.js
+++ tests/IsMyAnimal.spec.js
@@ -14,33 +14,23 @@
 var isMyAnimal = require('../src/is-my-animal');
 
 /**
- *  Cas 1 : La fonction doit retourner true si l'animal est un chat et false dans les autres cas
+ *  Cas 1 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanc" et false dans les autres cas
  */
 describe('À MODIFIER', () => {
   it('À MODIFIER', () => {
     expect(/** À MODIFIER **/).toEqual(/** À MODIFIER**/);
   })
 })
+
 /**
- *  Cas 2 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanc" et false dans les autres cas
+ * Cas 2 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanc" ou "BLANC" ou "Blanc" (peu importe la casse) et false dans les autres cas
  */
 
 /**
- * Cas 3 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanc" ou "BLANC" ou "Blanc" (peu importe la casse) et false dans les autres cas
+ * Cas 3 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanche" ou "BLANCHE" ou "Blanche" (peu importe la casse) et false dans les autres cas
  */
 
-/**
- * Cas 4 : La fonction doit retourner true aussi si l'animal est un chat, si la couleur du chat est "blanche" ou "BLANCHE" ou "Blanche" (peu importe la casse) et false dans les autres cas
- */
 
 /**
- * Cas 5 : La fonction doit retourner true aussi si le chat a des yeux du chat de couleur "vert" et false dans les autres cas
- */
-
-/**
- * Cas 6 : La fonction doit retourner true aussi si le chat a des yeux du chat de couleur "vert" ou "VERT" ou "Vert" (peu importe la casse) et false dans les autres cas
- */
-
-/**
- * Cas 7 : La fonction doit retourner true aussi si l'animal est un chien de couleur noir ou marron avec des yeux de couleur "vert" (peu importe la casse)
+ * Cas 4 : La fonction doit retourner true aussi si l'animal est un chien de couleur noir ou marron avec des yeux de couleur "vert" (peu importe la casse)
  */
